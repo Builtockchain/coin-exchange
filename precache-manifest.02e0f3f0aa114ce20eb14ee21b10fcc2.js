b60d9bf71a68c50141b1c8b7dd23f2981661b719
Supanut Jaisom <builtockchain@gmail.com>
2021-11-17 22:11:17 +0700

    Updates

---
+++ precache-manifest.02e0f3f0aa114ce20eb14ee21b10fcc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9bef121e6d8f213426d94c8cfcaa6e",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "e3739c8e340b3139a7d8",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "444c6653b52ea10e1f2e",
+    "url": "/coin-exchange/static/js/2.e614a097.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/coin-exchange/static/js/2.e614a097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf0906eee676f014eb5",
+    "url": "/coin-exchange/static/js/3.1de61f9a.chunk.js"
+  },
+  {
+    "revision": "e3739c8e340b3139a7d8",
+    "url": "/coin-exchange/static/js/main.498321c3.chunk.js"
+  },
+  {
+    "revision": "7cf8f50da603f6c19d04",
+    "url": "/coin-exchange/static/js/runtime-main.0871b07e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/coin-exchange/static/media/logo.06e73328.svg"
+  }
+]);
